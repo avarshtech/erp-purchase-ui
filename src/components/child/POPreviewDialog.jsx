37d5ef00d67f726f8b95aff25f46b0a3a3e10fa9
JRanjithKumar <ranjith@avarsh.com>
2026-01-29 19:36:42 +0530

    Warining Fix

--- src/components/child/POPreviewDialog.jsx
+++ src/components/child/POPreviewDialog.jsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
 import OperationControl from "../OperationControl";
 import { createActivity, updateActivity, parseActivityComment } from "../../services/poActivityLog";
